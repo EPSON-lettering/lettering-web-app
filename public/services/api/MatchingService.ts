9e2c3386ce07c8f341b1f0dc9b31cabb3f383375
galaxy4276 <deveungi@gmail.com>
2024-06-15 19:05:15 +0900

    feat: add check match api

---
+++ public/services/api/MatchingService.ts
@@ -0,0 +1,11 @@
+import { jsonClient } from "@public/services/api/client";
+
+interface MatchingService {
+	hasUserMatch: () => Promise<{ isMatch: boolean }>;
+}
+
+const matchingService: MatchingService = {
+	hasUserMatch: () => jsonClient.get(``),
+};
+
+export default matchingService;
--- public/services/api/index.ts
+++ public/services/api/index.ts
@@ -1,7 +1,9 @@
 import accountService from "@public/services/api/AccountService";
+import matchingService from "@public/services/api/MatchingService";
 
 const Server = {
 	Account: accountService,
+	Matching: matchingService,
 };
 
 export default Server;
