0915996c4aedea5600ebf81ce6452661901c792d
galaxy4276 <deveungi@gmail.com>
2024-06-16 05:16:32 +0900

    fix: revise ui bug

--- src/components/SplashLoader.tsx
+++ src/components/SplashLoader.tsx
@@ -1,22 +1,28 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import useSessionStore, { SessionItem } from "@/hooks/useSessionStore";
 import FirstLoading from "@/pages/FirstLoading";
 import Login from "@/pages/Login";
 import useUser from "@/hooks/useUser";
 import MatchRouter from "@/components/Matching/MatchRouter";
+import { useRouter } from "next/navigation";
 
 const SplashLoader = () => {
 	const { user } = useUser();
 	const sessionStore = useSessionStore();
+	const router = useRouter();
 	const splashDone: boolean = sessionStore.boolean(sessionStore.get(SessionItem.SPLASH));
+
+	useEffect(() => {
+		if (!user) return;
+		router.push('/match');
+	}, []);
 
 	return (
 			<>
-				{(!splashDone && !user) && <FirstLoading />}
-				{(splashDone && !user) && <Login />}
-				{user && <MatchRouter />}
+				{!splashDone && <FirstLoading />}
+				{splashDone && <Login />}
 			</>
 	);
 };
