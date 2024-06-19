0f178aad90c7b3a062633d83f3310f6cd3a28437
galaxy4276 <deveungi@gmail.com>
2024-06-19 10:16:49 +0900

    feat: LR-113, LR-115 #done

---
+++ public/services/api/printerService.ts
@@ -0,0 +1,16 @@
+import { jsonClient } from "@public/services/api/client";
+
+interface PrinterService {
+	print: (epsonEmail: string) => Promise<void>;
+	register: (epsonEmail: string) => Promise<void>;
+}
+
+
+const URL = '/printer';
+
+const printerService: PrinterService = {
+	print: (epsonEmail) => jsonClient.post(`${URL}/prints`, { deviceEmail: epsonEmail }),
+	register: (epsonEmail) => jsonClient.post(`${URL}/prints/auth`, { epsonEmail }),
+};
+
+export default printerService;
--- src/app/epson/register/page.tsx
+++ src/app/epson/register/page.tsx
@@ -2,13 +2,28 @@
 
 import Typo from "@/components/common/Typo";
 import NickInput from "@/components/common/NickInput";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "@/components/common/Button";
+import LeftArrow from "@public/icon/left-arrow-white.svg";
+import { useRouter } from "next/navigation";
+
+import Printer from "@public/icon/printer.svg";
+import LetterRound from "@public/icon/letter-logo-round.svg";
+import Processing from "@public/icon/processing.svg";
+import Server from "@public/services/api";
 
 export default function EpsonRegisterPage() {
 	const [epsonEmail, setEpsonEmail] = useState('');
+	const [connecting, setConnecting] = useState(false);
 
 	const onClickOpenHelp = () => window.open('https://www.epsonconnect.com/user');
+
+	const onClickRegisterEpsonPrinter = () => {
+		if (epsonEmail.trim() === '') return;
+		setConnecting(true);
+	};
+
+	if (connecting) return <Connecting email={epsonEmail} />;
 
 	return (
 			<div className="px-[16px] flex flex-col flex-1">
@@ -34,8 +49,12 @@ export default function EpsonRegisterPage() {
 
 				<section className="w-full flex justify-end pb-[60px]">
 					<Button
+						onClick={onClickRegisterEpsonPrinter}
 						shape="round"
 						size="fit"
+						icon={{
+							rightIcon: <LeftArrow />,
+						}}
 					>
 						다음
 					</Button>
@@ -43,3 +62,32 @@ export default function EpsonRegisterPage() {
 			</div>
 	);
 }
+
+const Connecting: React.FC<{ email: string }> = ({ email }) => {
+
+	useEffect(() => {
+		(async () => {
+			try {
+				await Server.Epson.register(email);
+				// await Server.Epson.print(email);
+			} catch (error) {
+				console.error(error);
+			}
+		})();
+	}, []);
+
+	return (
+			<div className="flex flex-1 flex-col px-[70px] w-full py-[200px]">
+				<section className="w-full col-center">
+					<Typo size="19" bold>프린터기를 연결 중입니다!</Typo>
+					<Typo size="19" bold>잠시만 기다려주세요 :)</Typo>
+				</section>
+
+				<section className="flex justify-center items-center gap-x-3 py-[130px] w-full flex-1">
+					<LetterRound />
+					<Processing />
+					<Printer />
+				</section>
+			</div>
+	)
+};
