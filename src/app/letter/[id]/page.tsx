3cd1628f0fadcbcf7b80e5b4c318203260c42e88
galaxy4276 <deveungi@gmail.com>
2024-06-24 08:03:37 +0900

    feat: letter image can download

--- src/app/letter/[id]/page.tsx
+++ src/app/letter/[id]/page.tsx
@@ -19,6 +19,13 @@ export default function LetterDetailsPage() {
 	});
 	const router = useRouter();
 
+	const onClickDownloadLetterImage = () => {
+		const tag = document.createElement('a') as HTMLAnchorElement;
+		tag.href = data?.imageUrl ?? '';
+		tag.download = data?.imageUrl ?? '';
+		tag.click();
+	};
+
 	if (!data || isLoading) return <Loading loading={isLoading} />;
 	const { owner: { nickname }, imageUrl } = data;
 
@@ -30,7 +37,7 @@ export default function LetterDetailsPage() {
 				</section>
 
 				<section className="flex-1 px-8 py-12 border rounded-2xl border-letter-yellow">
-					<img src={imageUrl} className="rounded-xl drop-shadow-lg" />
+					<img src={imageUrl} className="rounded-xl drop-shadow-lg cursor-pointer" onClick={onClickDownloadLetterImage} />
 				</section>
 
 				<section className="py-[60px] flex gap-x-4 px-12">
