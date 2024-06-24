c31349a535e442484253f54b0b770f8578a5916e
galaxy4276 <deveungi@gmail.com>
2024-06-25 06:40:14 +0900

    fix: do not call api, when not login

--- src/hooks/query/useNotificationsQuery.tsx
+++ src/hooks/query/useNotificationsQuery.tsx
@@ -2,9 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import Server from "@/services/api";
 
 const useNotificationsQuery = () => {
+	const isLogined = localStorage.getItem("access");
 	const { data = [], isLoading, ...props } = useQuery({
 		queryKey: ['notifications-getter'],
 		queryFn: Server.Notification.getList,
+		enabled: !!isLogined,
 	});
 
 	return {
