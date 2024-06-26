af4b02880024989c8dd6ef56cd2fd01b872c29ff
galaxy4276 <deveungi@gmail.com>
2024-06-26 13:00:39 +0900

    fix: prevent url

--- src/hooks/query/useMatchOneQuery.tsx
+++ src/hooks/query/useMatchOneQuery.tsx
@@ -2,13 +2,18 @@
 
 import { useQuery } from "@tanstack/react-query";
 import Server from "@/services/api";
+import { usePathname } from "next/navigation";
+
+const disabledUrls = ['/', '/sign-up'];
 
 const useMatchOneQuery = () => {
-	// const isLogined = localStorage.getItem("access");
+	const pathname = usePathname();
+	const enabled = !disabledUrls.includes(pathname ?? '');
+
 	const { isLoading, data = null, ...query } = useQuery({
 		queryKey: ['check-matching'],
 		queryFn: Server.Matching.getMyMatchingDetails,
-		// enabled: !!isLogined
+		enabled,
 	});
 
 	return {
